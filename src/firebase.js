b377d67f7148133fbdbd3853dfcf8d09f6330917
DomBart <domantas.bartusevicius@gmail.com>
2021-02-10 22:22:35 +0200

    init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCcqoypYPo7lg09LUSuiQH6Y3kpe5SvmZI",
+    authDomain: "netflix-clone-9268b.firebaseapp.com",
+    projectId: "netflix-clone-9268b",
+    storageBucket: "netflix-clone-9268b.appspot.com",
+    messagingSenderId: "923084529471",
+    appId: "1:923084529471:web:24cf0af9e4b046a90cbd8a"
+  };
