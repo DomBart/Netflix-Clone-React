c868b365cec178b68af770414b4f77640eb3c7fe
DomBart <domantas.bartusevicius@gmail.com>
2021-02-12 13:41:29 +0200

    Landing screen, Sign in / Sign up with firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import './App.css';
-import HomeScreen from "./components/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from './screens/LoginScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { auth } from './firebase';
 
 function App() {
+  const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        //Logged in
+      } else {
+        //Logged out
+      }
+    });
+
+    return unsubscribe;
+  }, [])
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ): (
+          <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+        )}
+    </Router>
     </div>
   );
 }
